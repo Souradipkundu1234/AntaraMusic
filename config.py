25a41af95d7471edaf57e43e0161fa457e0f7f22
ixamit007 <radhikaxmusicbot@gmail.com>
2025-10-28 23:04:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,73 +1,48 @@
 import re
 from os import getenv
-
 from dotenv import load_dotenv
 from pyrogram import filters
 
 load_dotenv()
 
-# Get this value from my.telegram.org/apps
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-
-# Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
-# -------------------------------------------------------
+
 OWNER_USERNAME = getenv("OWNER_USERNAME", "icxasta")
-# --------------------------------------------------------
 BOT_USERNAME = getenv("BOT_USERNAME", "odsmusicbot")
-# --------------------------------------------------------
 BOT_NAME = getenv("BOT_NAME")
-# ---------------------------------------------------------
 
-
-# Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
-
-# Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", -1002311769574))
-
-# Get this value from @PURVI_HELP_BOT on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 5909658683))
 
-# make your bots privacy from telegra.ph and put your url here 
 PRIVACY_LINK = getenv("PRIVACY_LINK", "https://graph.org/PRIVACY-FOR-TEAM-PURVI-BOTS-09-18")
 
-## Fill these variables if you're deploying on heroku.
-# Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-# Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-API_URL = getenv("API_URL", 'https://pytdbotapi.thequickearn.xyz') #youtube song url
+# ✅ Your working API key inserted here
+API_URL = getenv("API_URL", 'https://pytdbotapi.thequickearn.xyz')
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", 'NxGBNexGenBots4e1026') # youtube song api key
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots4e1026')
 
-
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/Souradipkundu1234/AntaraMusic",
-)
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Souradipkundu1234/AntaraMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GIT_TOKEN = getenv("GIT_TOKEN", None)  # Fill this variable if your upstream repository is private
+GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AntaraUpdates")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AntaraUpdates")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
-
-# Spotify credentials
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
-
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
-# Get your pyrogram v2 session from @StringFatherBot on Telegram
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
