0035393e0b3187161cae5a1637d5a6840ea2adf4
InSide Adnan <161193483+InSideAdnan@users.noreply.github.com>
2026-01-06 08:57:44 +0600

    Update Youtube.py

--- SHUKLAMUSIC/platforms/Youtube.py
+++ SHUKLAMUSIC/platforms/Youtube.py
@@ -1,16 +1,3 @@
-# ---------------------------------------------------------------
-# 🔸 Shashank YT-API Project
-# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
-# 📅 Copyright © 2025 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by ItzShukla
-# ---------------------------------------------------------------
 import asyncio
 import os
 import re
@@ -31,7 +18,7 @@ from SHUKLAMUSIC import LOGGER
 # =========================
 
 DOWNLOAD_DIR = "downloads"
-COOKIE_DIR = "AntaraMusic/assets/cookies"
+COOKIE_DIR = "SHUKLAMUSIC/assets/cookies"
 
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
